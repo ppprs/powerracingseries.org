f08999022c4aa575f5a2e2fd7523784214e7a00f
Josh Krueger <joshsinbox@gmail.com>
2014-02-16 00:44:53 -0600

    More CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  after_filter :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+  end
+
+
   def authenticate_admin!
     current_person.admin?
   end

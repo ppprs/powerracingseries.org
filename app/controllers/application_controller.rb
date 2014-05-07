b70c262fb27c93ecad8bb14091ea1aa7f83a0cf0
Josh Krueger <joshsinbox@gmail.com>
2014-05-06 21:33:15 -0500

    Access controll stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < ActionController::Base
   def set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(',')
+    headers['Access-Control-Max-Age'] = "1728000"
   end
 
 
